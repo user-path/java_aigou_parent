ccde43d58ca8ea1d9cf02302430af70dae428d33
user <1173238297@qq.com>
2019-03-29 23:03:04 +0800

    添加打包插件

---
+++ aigou_plat_parent/aigou_service_provide_login_8100/src/main/java/com/cn/aigou/domain/User.java
@@ -0,0 +1,65 @@
+package com.cn.aigou.domain;
+
+import com.baomidou.mybatisplus.enums.IdType;
+import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.activerecord.Model;
+import com.baomidou.mybatisplus.annotations.TableName;
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author zp
+ * @since 2019-03-29
+ */
+@TableName("t_user")
+public class User extends Model<User> {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    protected Serializable pkVal() {
+        return this.id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+        ", id=" + id +
+        ", username=" + username +
+        ", password=" + password +
+        "}";
+    }
+}
