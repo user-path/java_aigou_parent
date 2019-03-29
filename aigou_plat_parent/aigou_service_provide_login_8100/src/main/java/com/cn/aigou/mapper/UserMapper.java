399b54ffa93ff09f5c6720df3a2435e2c41e63e8
user <1173238297@qq.com>
2019-03-29 23:03:14 +0800

    添加打包插件

---
+++ aigou_plat_parent/aigou_service_provide_login_8100/src/main/java/com/cn/aigou/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.cn.aigou.mapper;
+
+import com.cn.aigou.domain.User;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author zp
+ * @since 2019-03-29
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
