f60ac652b8c10577ea90b6d3f7d70310e1fbd051
user <1173238297@qq.com>
2019-03-29 23:06:49 +0800

    service

---
+++ aigou_plat_parent/aigou_service_provide_login_8100/src/main/java/com/cn/aigou/service/IUserService.java
@@ -0,0 +1,16 @@
+package com.cn.aigou.service;
+
+import com.cn.aigou.domain.User;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author zp
+ * @since 2019-03-29
+ */
+public interface IUserService extends IService<User> {
+
+}
